90fabbfeafbdc9f7a1654f858ca8618027724cef
Mikael Aurell <aurell.mikael@gmail.com>
2019-12-09 11:40:16 +0100

    Add files via upload

---
+++ sbase_js.js
@@ -0,0 +1,48 @@
+"use strict";
+document.getElementById("welcomeH1").innerHTML = "Hello World Wide Web";
+
+var myDiv = document.getElementById("myDiv");
+
+myDiv.innerHTML = "";
+
+var myBody = document.getElementsByTagName("body")[0];
+
+myBody.style.backgroundColor = "lightgray";
+
+console.log(myDiv);
+
+myDiv.style.width = "19px";
+myDiv.style.height = "16px";
+myDiv.style.backgroundColor = "lightgreen";
+
+var chessToggle = true;
+var player = "X";
+
+for (let y = 1; y <= 3; y++) {
+  for (let x = 1; x <= 3; x++) {
+    const element = document.createElement("div");
+    element.className = "divBox";
+
+    if (chessToggle) {
+      element.classList.add("blackBox");
+    } else {
+      element.classList.add("whiteBox");
+    }
+    chessToggle = !chessToggle;
+    element.id = "x" + x + "y" + y;
+    element.addEventListener("click", playerMove);
+    myDiv.appendChild(element);
+  }
+  //logic if needed between rows
+}
+
+function playerMove(event) {
+  console.log(event);
+  event.target.innerHTML = player;
+
+  if (player === "X") {
+    player = "O";
+  } else {
+    player = "X";
+  }
+}
